7b38c62c46f3c32e1cd769ee9f65a8ae6ae8989d
Welber Serafim <serafimwelber@gmail.com>
2024-09-21 18:20:11 -0300

    include Auth config and Jwt Service token

--- src/main/java/br/com/gearing/guru/adapters/repository/UserRepository.java
+++ src/main/java/br/com/gearing/guru/adapters/repository/UserRepository.java
@@ -1,8 +1,16 @@
 package br.com.gearing.guru.adapters.repository;
 
 
+import br.com.gearing.guru.domain.model.users.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long > {
+
+    Optional<User> findByEmail(String email);
+
 }
